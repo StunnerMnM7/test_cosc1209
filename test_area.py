37cddd634992fa3611c920ec0364f56bde9308c3
Mihir Mane <mihirmane3@gmail.com>
2025-02-23 12:02:41 -0500

    changes for failure

--- test_area.py
+++ test_area.py
@@ -3,13 +3,15 @@ from area import calculate_area_square
 
 def test_calculate_area_square_negative():  
     with pytest.raises(TypeError):  
-        calculate_area_square(-2)  
+        calculate_area_square(-80)  
 
 def test_calculate_area_square_string():  
     with pytest.raises(TypeError):  
-        calculate_area_square("2")  
+        calculate_area_square("80")  
 
 def test_calculate_area_square_list():  
     with pytest.raises(TypeError):  
-        calculate_area_square([2])  
-
+        calculate_area_square([80])  
+        
+def test_calculate_area_square_failure():  
+    assert calculate_area_square(80) == 160
