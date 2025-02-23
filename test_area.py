e3843a4c156954342a1238c10a3f8976e1c012fc
Mihir Mane <mihirmane3@gmail.com>
2025-02-23 11:46:59 -0500

    .

--- test_area.py
+++ test_area.py
@@ -1,14 +1,15 @@
 import pytest  
-from src.area import calculate_area_square  
-  
+from area import calculate_area_square
+
 def test_calculate_area_square_negative():  
     with pytest.raises(TypeError):  
         calculate_area_square(-2)  
-  
+
 def test_calculate_area_square_string():  
     with pytest.raises(TypeError):  
         calculate_area_square("2")  
-  
+
 def test_calculate_area_square_list():  
     with pytest.raises(TypeError):  
-        calculate_area_square([2])
+        calculate_area_square([2])  
+
